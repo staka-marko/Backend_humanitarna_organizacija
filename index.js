7f846ae97b14bba97182520bc9f2597648b7b374
= <staka.99@hotmail.com>
2022-05-12 01:07:53 +0200

    Backend dio aplikacije

---
+++ index.js
@@ -0,0 +1,448 @@
+const mysql = require('mysql');
+var express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+
+var PORT = 3000;
+
+var mysqlConnection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password: 'root',
+    database : 'hodb',
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err)=>{
+    if(!err)
+    console.log('DB konekcija uspješna.');
+    else 
+    console.log('DB konekcija neuspješna \n Greška : ' + JSON.stringify(err, undefined, 2));
+});
+
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+}); 
+  
+app.get('/', (req, res) => {
+    res.send('hello world')
+  });
+
+
+//------------------------------- TABELA 1 - GRAD ---------------------------------------------------------
+
+// Get all gradovi  
+app.get('/grad',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.grad;',(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Get grad
+app.get('/grad/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.grad WHERE gradID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Delete grad
+app.delete('/grad/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM hodb.grad WHERE gradID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno brisanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Insert grad
+app.post('/grad',(req, res)=>{
+    mysqlConnection.query('INSERT INTO hodb.grad (gradID, nazivGrada, postanskiBroj) VALUES (?,?,?)', [req.body.gradID, req.body.nazivGrada, req.body.postanskiBroj],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno dodavanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Update grad
+app.put('/grad',(req, res)=>{
+    mysqlConnection.query('UPDATE hodb.grad SET gradID = ?, nazivGrada = ?, postanskiBroj = ? WHERE gradID = ?', [req.body.gradID, req.body.nazivGrada, req.body.postanskiBroj, req.body.gradID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješna izmjena.');
+        else
+        console.log(err);
+    })
+});
+
+//------------------------------- TABELA 2 - CLANARINA ---------------------------------------------------------
+
+// Get all clanarina  
+app.get('/clanarina',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.clanarina;',(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Get clanarina
+app.get('/clanarina/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.clanarina WHERE clanarinaID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Delete clanarina
+app.delete('/clanarina/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM hodb.clanarina WHERE clanarinaID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno brisanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Insert clanarina
+app.post('/clanarina',(req, res)=>{
+    mysqlConnection.query('INSERT INTO hodb.clanarina (clanarinaID, vrstaClanarine, iznos) VALUES (?,?,?)', [req.body.clanarinaID, req.body.vrstaClanarine, req.body.iznos],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno dodavanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Update clanarina
+app.put('/clanarina',(req, res)=>{
+    mysqlConnection.query('UPDATE hodb.clanarina SET clanarinaID = ?, vrstaClanarine = ?, iznos = ? WHERE clanarinaID = ?', [req.body.clanarinaID, req.body.vrstaClanarine, req.body.iznos, req.body.clanarinaID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješna izmjena.');
+        else
+        console.log(err);
+    })
+});
+
+//------------------------------- TABELA 3 - OCUVANOST ---------------------------------------------------------
+
+// Get all ocuvanost  
+app.get('/ocuvanost',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.ocuvanost;',(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Get ocuvanost
+app.get('/ocuvanost/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.ocuvanost WHERE ocuvanostID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Delete ocuvanost
+app.delete('/ocuvanost/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM hodb.ocuvanost WHERE ocuvanostID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno brisanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Insert ocuvanost
+app.post('/ocuvanost',(req, res)=>{
+    mysqlConnection.query('INSERT INTO hodb.ocuvanost (ocuvanostID, ocuvanostOdjece) VALUES (?,?)', [req.body.ocuvanostID, req.body.ocuvanostOdjece],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno dodavanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Update ocuvanost
+app.put('/ocuvanost',(req, res)=>{
+    mysqlConnection.query('UPDATE hodb.ocuvanost SET ocuvanostID = ?, ocuvanostOdjece = ? WHERE ocuvanostID = ?', [req.body.ocuvanostID, req.body.ocuvanostOdjece, req.body.ocuvanostID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješna izmjena.');
+        else
+        console.log(err);
+    })
+});
+
+//------------------------------- TABELA 4 - VRSTA_ODJECE ---------------------------------------------------------
+
+// Get all vrsta_odjece  
+app.get('/vrsta_odjece',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.vrsta_odjece;',(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Get vrsta_odjece
+app.get('/vrsta_odjece/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.vrsta_odjece WHERE vrstaOdjeceID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Delete vrsta_odjece
+app.delete('/vrsta_odjece/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM hodb.vrsta_odjece WHERE vrstaOdjeceID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno brisanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Insert vrsta_odjece
+app.post('/vrsta_odjece',(req, res)=>{
+    mysqlConnection.query('INSERT INTO hodb.vrsta_odjece (vrstaOdjeceID, nazivVrste) VALUES (?,?)', [req.body.vrstaOdjeceID, req.body.nazivVrste],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno dodavanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Update vrsta_odjece
+app.put('/vrsta_odjece',(req, res)=>{
+    mysqlConnection.query('UPDATE hodb.vrsta_odjece SET vrstaOdjeceID = ?, nazivVrste = ? WHERE vrstaOdjeceID = ?', [req.body.vrstaOdjeceID, req.body.nazivVrste, req.body.vrstaOdjeceID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješna izmjena.');
+        else
+        console.log(err);
+    })
+});
+
+//------------------------------- TABELA 5 - ADRESA ---------------------------------------------------------
+
+// Get all adresa  
+app.get('/adresa',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.adresa;',(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Get adresa
+app.get('/adresa/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.adresa WHERE adresaID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Delete adresa
+app.delete('/adresa/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM hodb.adresa WHERE adresaID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno brisanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Insert adresa
+app.post('/adresa',(req, res)=>{
+    mysqlConnection.query('INSERT INTO hodb.adresa (adresaID, ulica, broj, sprat, brojStana, gradID) VALUES (?,?,?,?,?,?)', [req.body.adresaID, req.body.ulica, req.body.broj, req.body.sprat,  req.body.brojStana, req.body.gradID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno dodavanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Update adresa
+app.put('/adresa',(req, res)=>{
+    mysqlConnection.query('UPDATE hodb.adresa SET adresaID = ?, ulica = ?, broj = ?, sprat = ?, brojStana = ?, gradID = ? WHERE adresaID = ?', [req.body.adresaID, req.body.ulica, req.body.broj, req.body.sprat,  req.body.brojStana, req.body.gradID, req.body.adresaID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješna izmjena.');
+        else
+        console.log(err);
+    })
+});
+
+//------------------------------- TABELA 6 - KORISNIK ---------------------------------------------------------
+
+// Get all korisnik  
+app.get('/korisnik',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.korisnik;',(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Get korisnik
+app.get('/korisnik/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.korisnik WHERE korisnikID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Delete korisnik
+app.delete('/korisnik/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM hodb.korisnik WHERE korisnikID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno brisanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Insert korisnik
+app.post('/korisnik',(req, res)=>{
+    mysqlConnection.query('INSERT INTO hodb.korisnik (korisnikID, ime, prezime, jmbg, datumRodjenja, pocetakVolontiranja, email, lozinka, adresaID, clanarinaID, uloga) VALUES (?,?,?,?,?,?,?,?,?,?,?)', [req.body.korisnikID, req.body.ime, req.body.prezime, req.body.jmbg, req.body.datumRodjenja, req.body.pocetakVolontiranja, req.body.email, req.body.lozinka, req.body.adresaID, req.body.clanarinaID, req.body.uloga],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno dodavanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Update korisnik
+app.put('/korisnik',(req, res)=>{
+    mysqlConnection.query('UPDATE hodb.korisnik SET korisnikID = ?, ime = ?, prezime = ?, jmbg = ?, datumRodjenja = ?, pocetakVolontiranja = ?, email = ?, lozinka = ?, adresaID = ?, clanarinaID = ?, uloga = ? WHERE korisnikID = ?', [req.body.korisnikID, req.body.ime, req.body.prezime, req.body.jmbg,  req.body.datumRodjenja, req.body.pocetakVolontiranja, req.body.email, req.body.lozinka, req.body.adresaID, req.body.clanarinaID, req.body.uloga, req.body.korisnikID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješna izmjena.');
+        else
+        console.log(err);
+    })
+});
+
+//------------------------------- TABELA 7 - REZERVACIJA ---------------------------------------------------------
+
+// Get all rezervacija  
+app.get('/rezervacija',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.rezervacija;',(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Get rezervacija
+app.get('/rezervacija/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.rezervacija WHERE rezervacijaID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Delete rezervacija
+app.delete('/rezervacija/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM hodb.rezervacija WHERE rezervacijaID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno brisanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Insert rezervacija
+app.post('/rezervacija',(req, res)=>{
+    mysqlConnection.query('INSERT INTO hodb.rezervacija (rezervacijaID, dan, datum, vrijeme, korisnikID, volonterID) VALUES (?,?,?,?,?,?)', [req.body.rezervacijaID, req.body.dan, req.body.datum, req.body.vrijeme, req.body.korisnikID, req.body.volonterID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno dodavanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Update rezervacija
+app.put('/rezervacija',(req, res)=>{
+    mysqlConnection.query('UPDATE hodb.rezervacija SET rezervacijaID = ?, dan = ?, datum = ?, vrijeme = ?, korisnikID = ?, volonterID = ? WHERE rezervacijaID = ?', [req.body.rezervacijaID, req.body.dan, req.body.datum, req.body.vrijeme, req.body.korisnikID, req.body.volonterID, req.body.rezervacijaID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješna izmjena.');
+        else
+        console.log(err);
+    })
+});
+
+//------------------------------- TABELA 8 - KOMAD_ODJECE ---------------------------------------------------------
+
+// Get all komad_odjece  
+app.get('/komad_odjece',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.komad_odjece;',(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Get komad_odjece
+app.get('/komad_odjece/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM hodb.komad_odjece WHERE komadID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+// Delete komad_odjece
+app.delete('/komad_odjece/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM hodb.komad_odjece WHERE komadID = ?', [req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno brisanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Insert komad_odjece
+app.post('/komad_odjece',(req, res)=>{
+    mysqlConnection.query('INSERT INTO hodb.komad_odjece (komadID, velicina, boja, pol, rezervacijaID, vrstaID, ocuvanostID) VALUES (?,?,?,?,?,?,?)', [req.body.komadID, req.body.velicina, req.body.boja, req.body.pol, req.body.rezervacijaID, req.body.vrstaID, req.body.ocuvanostID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješno dodavanje.');
+        else
+        console.log(err);
+    })
+});
+
+// Update komad_odjece
+app.put('/komad_odjece',(req, res)=>{
+    mysqlConnection.query('UPDATE hodb.komad_odjece SET komadID = ?, velicina = ?, boja = ?, pol = ?, rezervacijaID = ?, vrstaID = ?, ocuvanostID = ? WHERE komadID = ?', [req.body.komadID, req.body.velicina, req.body.boja, req.body.pol, req.body.rezervacijaID, req.body.vrstaID, req.body.ocuvanostID, req.body.komadID],(err, rows, fields)=>{
+        if(!err)
+        res.send('Uspješna izmjena.');
+        else
+        console.log(err);
+    })
+});
